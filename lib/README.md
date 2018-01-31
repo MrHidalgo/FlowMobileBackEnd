73630c7d875719dae1f9f9253d67f9a4b34013a6
MrHidalgo <Luckyhidalgo@gmail.com>
2018-01-31 10:45:08 +0200

    Initial commit

---
+++ lib/README.md
@@ -0,0 +1,1 @@
+## Back-End helpers files/script
---
+++ server.js
@@ -0,0 +1,8 @@
+﻿const http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello Ionic! To beat shit out someone');
+
+}).listen(process.env.PORT || 8080);
