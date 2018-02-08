0b8c63837e84063be1afd64de8c76aea616e85b5
MrHidalgo <Luckyhidalgo@gmail.com>
2018-02-08 10:54:46 +0200

    Add configuration package, create express.js server

--- server.js
+++ server.js
@@ -1,8 +1,53 @@
-﻿const http = require('http');
+﻿// =======================
+// GET THE PACKAGE WE NEED
+// =======================
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
+const bodyParser = require('body-parser');
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello Ionic! To beat shit out someone');
+const jwt = require('jsonwebtoken');
 
-}).listen(process.env.PORT || 8080);
+const config = require('./config');
+
+
+// =======================
+// Configuration
+// =======================
+const port = process.env.PORT || 8080;
+app.set('superSecret', config.secret);
+
+
+// =======================
+// Use body parser so we can get info from POST and/or URL parameters
+// =======================
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
+
+// =======================
+// Routes
+// =======================
+app.get('/', (req, res) => {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+
+// =======================
+// Start the server
+// =======================
+app.listen(port);
+
+
+
+// const http = require('http');
+//
+// http.createServer(function (req, res) {
+//
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Hello Ionic! To beat shit out someone');
+//
+// }).listen(process.env.PORT || 8080);
