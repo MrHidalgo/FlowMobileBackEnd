d50e305cc791777579869bf36cef3a95d047852c
MrHidalgo <Luckyhidalgo@gmail.com>
2018-02-15 21:26:53 +0200

    Setting server - v0.3

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const bodyParser    = require('body-parser');
 // const jwt           = require('jsonwebtoken');
 
-// const config        = require('./config');
-// const _logger       = require('./lib/_logger');
+const config        = require('./config');
+const _logger       = require('./lib/_logger');
 
 const app           = express();
 
@@ -20,14 +20,14 @@ const port = process.env.PORT || 8080;
 /**
  * @description Secret variable
  */
-// app.set('superSecret', config.SECRET);
+app.set('superSecret', config.SECRET);
 
 
 /**
  * @description Create a write stream (in append mode) - HTTP request logger middleware
  * Use morgan npm to log requests to the file.
  */
-// app.use(_logger.morganLogger);
+app.use(_logger.morganLogger);
 
 
 /**
@@ -52,75 +52,75 @@ app.get('/', (req, res) => {
 /**
  * @description get an instance of the router for api routes
  */
-// const apiRoutes     = express.Router();
-//
-//
-// apiRoutes.get('/auth', (req, res) => {
-//     res.send('Auth API...');
-// });
-// apiRoutes.get('/profile', (req, res) => {
-//     res.send('Profile API...');
-// });
-// apiRoutes.get('/task', (req, res) => {
-//     res.send('Task API...');
-// });
-// apiRoutes.get('/contacts', (req, res) => {
-//     res.send('Contacts API...');
-// });
-// apiRoutes.get('/hours', (req, res) => {
-//     res.send('Hours API...');
-// });
-// apiRoutes.get('/setting', (req, res) => {
-//     res.send('Setting API...');
-// });
+const apiRoutes     = express.Router();
+
+
+apiRoutes.get('/auth', (req, res) => {
+    res.send('Auth API...');
+});
+apiRoutes.get('/profile', (req, res) => {
+    res.send('Profile API...');
+});
+apiRoutes.get('/task', (req, res) => {
+    res.send('Task API...');
+});
+apiRoutes.get('/contacts', (req, res) => {
+    res.send('Contacts API...');
+});
+apiRoutes.get('/hours', (req, res) => {
+    res.send('Hours API...');
+});
+apiRoutes.get('/setting', (req, res) => {
+    res.send('Setting API...');
+});
 
 
 /**
  * @description Apply the routes to our application with the prefix /api
  */
-// app.use('/', apiRoutes);
-//
-// app.use((req, res, next) => {
-//     let err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-//
-// app.use(function(err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//
-//     // render the error page
-//     res.status(err.status || 500);
-//
-//     if(err.status === 404){
-//         res.format({
-//             'text/plain': () => {
-//                 res.send({
-//                     message: 'Not Found Data'
-//                 });
-//             },
-//             'text/html': () => {
-//                 res.send("404: Sorry can't find that!");
-//             },
-//             'application/json': () => {
-//                 res.send({
-//                     message: 'Not Found Data'
-//                 });
-//             },
-//             'default': () => {
-//                 res.status(406).send('Not Acceptable');
-//             }
-//         })
-//     }
-//
-//     // when status is 500, error handler
-//     if(err.status === 500) {
-//         return res.send({
-//             message: 'Error Occur'
-//         });
-//     }
-// });
+app.use('/', apiRoutes);
+
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+
+    // render the error page
+    res.status(err.status || 500);
+
+    if(err.status === 404){
+        res.format({
+            'text/plain': () => {
+                res.send({
+                    message: 'Not Found Data'
+                });
+            },
+            'text/html': () => {
+                res.send("404: Sorry can't find that!");
+            },
+            'application/json': () => {
+                res.send({
+                    message: 'Not Found Data'
+                });
+            },
+            'default': () => {
+                res.status(406).send('Not Acceptable');
+            }
+        })
+    }
+
+    // when status is 500, error handler
+    if(err.status === 500) {
+        return res.send({
+            message: 'Error Occur'
+        });
+    }
+});
 
 
 /**
