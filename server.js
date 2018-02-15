cdd52f9eede5845df61d2347716e425aee842a58
MrHidalgo <Luckyhidalgo@gmail.com>
2018-02-15 21:40:06 +0200

    Setting server - v0.5

--- server.js
+++ server.js
@@ -27,16 +27,16 @@ app.set('superSecret', config.SECRET);
  * @description Create a write stream (in append mode) - HTTP request logger middleware
  * Use morgan npm to log requests to the file.
  */
-app.use(_logger.morganLogger);
+// app.use(_logger.morganLogger);
 
 
 /**
  * @description Use body parser so we can get info from POST and/or URL parameters
  */
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+// app.use(bodyParser.json());
 
 
 /**
@@ -80,48 +80,48 @@ app.get('/', (req, res) => {
  */
 // app.use('/', apiRoutes);
 
-app.use((req, res, next) => {
-    let err = new Error('Not Found');
-    
-    err.status = 404;
-    next(err);
-});
-
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-
-    // render the error page
-    res.status(err.status || 500);
-
-    if(err.status === 404){
-        res.format({
-            'text/plain': () => {
-                res.send({
-                    message: 'Not Found Data'
-                });
-            },
-            'text/html': () => {
-                res.send("404: Sorry can't find that!");
-            },
-            'application/json': () => {
-                res.send({
-                    message: 'Not Found Data'
-                });
-            },
-            'default': () => {
-                res.status(406).send('Not Acceptable');
-            }
-        })
-    }
-
-    // when status is 500, error handler
-    if(err.status === 500) {
-        return res.send({
-            message: 'Error Occur'
-        });
-    }
-});
+// app.use((req, res, next) => {
+//     let err = new Error('Not Found');
+//
+//     err.status = 404;
+//     next(err);
+// });
+//
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//
+//     if(err.status === 404){
+//         res.format({
+//             'text/plain': () => {
+//                 res.send({
+//                     message: 'Not Found Data'
+//                 });
+//             },
+//             'text/html': () => {
+//                 res.send("404: Sorry can't find that!");
+//             },
+//             'application/json': () => {
+//                 res.send({
+//                     message: 'Not Found Data'
+//                 });
+//             },
+//             'default': () => {
+//                 res.status(406).send('Not Acceptable');
+//             }
+//         })
+//     }
+//
+//     // when status is 500, error handler
+//     if(err.status === 500) {
+//         return res.send({
+//             message: 'Error Occur'
+//         });
+//     }
+// });
 
 
 /**
