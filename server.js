4cd1f076e8ed7d8643cdcaaa002c06e16e04615f
MrHidalgo <Luckyhidalgo@gmail.com>
2018-02-15 23:09:38 +0200

    Setting server - v1.5

--- server.js
+++ server.js
@@ -80,48 +80,62 @@ apiRoutes.get('/setting', (req, res) => {
  */
 app.use('/api', apiRoutes);
 
-app.use((req, res, next) => {
-    let err = new Error('Not Found');
-
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+    res
+        .status(404)
+        .send('404: Sorry cant find that!');
 });
 
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-
-    // render the error page
-    res.status(err.status || 500);
-
-    if(err.status === 404){
-        res.format({
-            'text/plain': () => {
-                res.send({
-                    message: 'Not Found Data'
-                });
-            },
-            'text/html': () => {
-                res.send("404: Sorry can't find that!");
-            },
-            'application/json': () => {
-                res.send({
-                    message: 'Not Found Data'
-                });
-            },
-            'default': () => {
-                res.status(406).send('Not Acceptable');
-            }
-        })
-    }
-
-    // when status is 500, error handler
-    if(err.status === 500) {
-        return res.send({
-            message: 'Error Occur'
-        });
-    }
+    res
+        .status(500)
+        .send('500: Something broke!');
 });
+
+//
+// app.use((req, res, next) => {
+//     let err = new Error('Not Found');
+//
+//     err.status = 404;
+//
+//     next(err);
+// });
+//
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//
+//     if(err.status === 404){
+//         res.format({
+//             'text/plain': () => {
+//                 res.send({
+//                     message: 'Not Found Data'
+//                 });
+//             },
+//             'text/html': () => {
+//                 res.send("404: Sorry can't find that!");
+//             },
+//             'application/json': () => {
+//                 res.send({
+//                     message: 'Not Found Data'
+//                 });
+//             },
+//             'default': () => {
+//                 res.status(406).send('Not Acceptable');
+//             }
+//         })
+//     }
+//
+//     // when status is 500, error handler
+//     if(err.status === 500) {
+//         return res.send({
+//             message: 'Error Occur'
+//         });
+//     }
+// });
 
 
 /**
